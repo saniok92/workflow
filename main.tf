129a75bb6de656511ee4d509412ec007b8aea5d7
saniok92 <kovalsaniok@gmail.com>
2021-06-03 15:35:59 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
     }
   }
 }
-variable "TERRAFORM_VAR_DO_TOKEN" {}
+variable "do_token" {}
 
 provider "digitalocean" {
-  token = TERRAFORM_VAR_DO_TOKEN
+  token = var.do_token
 }
 
 # terraform kubernetes claster
