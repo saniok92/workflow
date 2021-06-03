69eecc7217d87f6f732066bcd267816a58dc1c2d
saniok92 <kovalsaniok@gmail.com>
2021-06-03 14:59:49 +0300

    add terraform files

---
+++ claster.tf
@@ -0,0 +1,16 @@
+variable "do_claster_name" {
+  default = "koval"
+}
+
+resource "digitalocean_kubernetes_cluster" "koval" {
+  name    = "koval"
+  region  = "fra1"
+  version = "1.20.2-do.0"
+
+  node_pool {
+    name       = "test-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 1
+  }
+}
+
