fdca8dfcc8fa7d708389882516d535886123b87d
saniok92 <kovalsaniok@gmail.com>
2021-06-03 15:22:04 +0300

    add main.tf dell provider.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+#terraform provider 
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "1.22.2"
+    }
+  }
+}
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# terraform kubernetes claster
 variable "do_claster_name" {
   default = "koval"
 }
