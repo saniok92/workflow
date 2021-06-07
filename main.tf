a36cabad707c480a0c1bb0540011dffd3eb5c253
saniok92 <kovalsaniok@gmail.com>
2021-06-07 16:21:21 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "1.22.2"
+    }
+  }
+}
+
+variable "do_token" {}
+variable "pvt_key" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_ssh_key" "terraform" {
+  name = "ubuntu"
+}
+
+
+
 resource "digitalocean_droplet" "koval" {
   image = "ubuntu-20-04-x64"
   name = "koval"
