49eadc7f5c3f7a831cef8cece0fde3517e1eb99b
saniok92 <kovalsaniok@gmail.com>
2021-06-07 16:42:59 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -2,76 +2,34 @@ terraform {
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
-      version = "1.22.2"
+      version = "~> 2.0"
     }
   }
 }
 
 variable "do_token" {}
-variable "pvt_key" {}
 
 provider "digitalocean" {
-  token = var.do_token
+  digitalocean_token = var.do_token
 }
 
-data "digitalocean_ssh_key" "terraform" {
-  name = "ubuntu"
+variable "do_claster_name" {
+  default = "koval"
 }
 
+resource "digitalocean_kubernetes_cluster" "koval" {
+  name    = "koval"
+  region  = "fra1"
+  version = "1.20.2-do.0"
 
-
-resource "digitalocean_droplet" "koval" {
-  image = "ubuntu-20-04-x64"
-  name = "koval"
-  region = "fra1"
-  size = "s-1vcpu-1gb"
-  private_networking = true
-  ssh_keys = [
-    data.digitalocean_ssh_key.terraform.id
-  ]
-  connection {
-    host = self.ipv4_address
-    user = "root"
-    type = "ssh"
-    private_key = file(var.pvt_key)
-    timeout = "2m"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      # install nginx
-      "sudo apt-get update",
-      "sudo apt-get -y install nginx",
-      "sudo apt install nano",
-      "touch elementary.txt"
-    ]
+  node_pool {
+    name       = "test-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 1
   }
 }
 
-resource "digitalocean_droplet" "koval1" {
-  image = "ubuntu-20-04-x64"
-  name = "koval1"
-  region = "fra1"
-  size = "s-1vcpu-1gb"
-  private_networking = true
-  ssh_keys = [
-    data.digitalocean_ssh_key.terraform.id
-  ]
-  connection {
-    host = self.ipv4_address
-    user = "root"
-    type = "ssh"
-    private_key = file(var.pvt_key)
-    timeout = "2m"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      # install nginx
-      "sudo apt-get update",
-      "sudo apt-get -y install nginx",
-      "sudo apt install nano",
-      "touch elementary.txt"
-    ]
-  }
-}
+
+
+
+
