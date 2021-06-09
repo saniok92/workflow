096d7394d9b85dbce04e298b4487e8ca6872991e
saniok92 <kovalsaniok@gmail.com>
2021-06-09 14:18:45 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -8,17 +8,20 @@ terraform {
 }
 
 variable "do_token" {
-  type = string
 }
 
 provider "digitalocean" {
   token = var.do_token
 }
 
+variable "do_claster_name" {
+  default = "koval"
+}
+
 resource "digitalocean_kubernetes_cluster" "koval" {
   name    = "koval"
   region  = "fra1"
-  version = "1.20.2-do.0"
+  version = "1.20.7-do.0"
 
   node_pool {
     name       = "test-pool"
@@ -26,5 +29,7 @@ resource "digitalocean_kubernetes_cluster" "koval" {
     node_count = 1
   }
 }
+
+
 
 
