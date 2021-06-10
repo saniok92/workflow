377533770548115a5ad3ec580a0f9414ba0faa23
saniok92 <kovalsaniok@gmail.com>
2021-06-10 13:57:09 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "2.9.0"
     }
   }
@@ -15,17 +15,17 @@ provider "digitalocean" {
 
 variable "do_claster_name" {
   default = "koval"
-} 
+}
 
 resource "digitalocean_kubernetes_cluster" "koval" {
   name    = "koval"
   region  = "fra1"
   version = "1.20.7-do.0"
 
-    node_pool {
-    name       = "koval"
-    size       = "s-1vcpu-2gb" 
-    auto_scale = false
+  node_pool {
+    name       = "test-pool"
+    size       = "s-1vcpu-2gb"
     node_count = 1
   }
 }
+
