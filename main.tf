21c7b2c07c7a38f16a99dc98dbffc0b96ef5d2f3
saniok92 <kovalsaniok@gmail.com>
2021-06-10 09:18:51 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -13,21 +13,19 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+
 variable "do_claster_name" {
-  default = "koval"
+    default = "koval"
 }
 resource "digitalocean_kubernetes_cluster" "koval" {
   name    = "koval"
   region  = "fra1"
   version = "1.20.7-do.0"
 
-  node_pool {
-    name       = "test-pool"
-    size       = "s-1vcpu-2gb"
+    node_pool {
+    name       = "koval"
+    size       = "s-1vcpu-2gb" 
+    auto_scale = false
     node_count = 1
   }
 }
-
-
-
-
